4aaecf39aa7264124bceb0a7554b922cb2f35e70
BladeCode <Jerry.x@outlook.com>
2018-11-19 01:51:08 +0800

    init project

---
+++ README.md
@@ -0,0 +1,49 @@
+# rc-android-streamer
+
+streamer是一款类banner开源项目
+* 支持无线轮播
+* 支持自定义指示器
+* 支持View,Fragment两种模式
+* 支持Card是否圆角，及圆角的大小
+
+
+```
+rc-cluster-streamer
+    ├── sample/           # 使用示例
+    │   ├──
+    │   ├──
+    │   └──
+    │
+    ├── streamer/         # 功能实现
+    │   ├──
+    │   ├──
+    │   └──
+    │
+    ├── README.md
+    └── LICENSE
+```
+
+## How do I use Matisse?
+
+## Thanks
+
+* [CardSlideView](https://github.com/crazysunj/CardSlideView)
+* [BezierViewPager](https://github.com/qdxxxx/BezierViewPager)
+* [ViewPagerCards](https://github.com/rubensousa/ViewPagerCards)
+
+## License
+```
+Copyright (C) 2018 The Jerry xu Open Source Project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ streamer/src/main/java/org/incoder/streamer/internal/CardFragmentPagerAdapter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2018 The Jerry xu Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.incoder.streamer.internal;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * CardFragmentPagerAdapter.
+ *
+ * @author : Jerry xu
+ * @since : 2018/11/16 00:32
+ */
+public class CardFragmentPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<CardFragment> mFragments;
+    private float mBaseElevation;
+
+    public CardFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
